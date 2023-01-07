089d2567c41bc7154ce3b2ef73473404a7a208e8
unknown <demomusicevcat@gmail.com>
2023-01-07 10:23:13 +0100

    removed taskReducer

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,9 +1,5 @@
-import { UPDATE_STATE, ADD_TASK } from '../reducers/tasksReducer';
 import { UPDATE_MODAL_OPTIONS } from '../reducers/modalReducer';
 import { ADD_TASK_DATA, CLEAR_TASK_DATA, UPDATE_TASK_DETAILS_OPEN } from '../reducers/taskDetailsReducer';
-
-export const updateTasksAction = (payload) => ({type: UPDATE_STATE, payload});
-export const addTaskAction = (payload) => ({type: ADD_TASK, payload});
 
 export const updateModalAction = () => ({type: UPDATE_MODAL_OPTIONS}); 
 
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import { tasksReducer } from './tasksReducer';
 import { modalReducer } from './modalReducer'
 import { taskDetailsReducer } from './taskDetailsReducer';
 
 
 export const rootReducer = combineReducers({
-    tasks: tasksReducer,
     modal: modalReducer,
     taskDetails: taskDetailsReducer, 
 })
